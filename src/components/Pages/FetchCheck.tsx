606edd12cd34992e602dfc239eac70d5ab822aad
Hiroharu Tanaka <hiroharu8864@gol.com>
2022-06-21 16:31:43 +0900

    add fetch page

--- src/components/Pages/FetchCheck.tsx
+++ src/components/Pages/FetchCheck.tsx
@@ -1,5 +1,29 @@
-import { FC, memo, useCallback } from "react";
+import { FC, memo, useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+type Users = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
 
 export const FetchCheck: FC = memo(() => {
   const navigate = useNavigate();
@@ -7,9 +31,27 @@ export const FetchCheck: FC = memo(() => {
     navigate("/");
   }, [navigate]);
 
+  const fetchUsers = async () => {
+    const resonse = await fetch("https://jsonplaceholder.typicode.com/users");
+    return resonse.json();
+  };
+
+  const [users, setUsers] = useState<Array<Users>>([]);
+
+  useEffect(() => {
+    fetchUsers().then((data) => {
+      setUsers(data);
+    });
+  }, []);
+
   return (
     <>
-      <p>This is Fetch Check Page</p>
+      <p>Fetchユーザ一覧</p>
+      <div>
+        {users.map((user) => (
+          <div key={user.id}>{user.name}</div>
+        ))}
+      </div>
       <button onClick={onClickHome}>to home</button>
     </>
   );
