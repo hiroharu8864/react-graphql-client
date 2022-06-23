2c6fdee62e10143f1c68301a76e731b07e6feae3
Hiroharu Tanaka <hiroharu8864@gol.com>
2022-06-23 09:58:41 +0900

    graphQLの返り値がundefined

--- src/components/Pages/QueryCheckGraphQL.tsx
+++ src/components/Pages/QueryCheckGraphQL.tsx
@@ -1,11 +1,37 @@
 import { FC, memo, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
+import { useQuery } from "react-query";
+import { GraphQLClient, gql } from "graphql-request";
+
+let url: string = "https://api.github.com/graphql";
+let accessKey: string = "";
+
+const graphQLClient = new GraphQLClient(url, {
+  headers: {
+    Authorization: `bearer ${accessKey}`
+  }
+});
 
 export const QueryCheckGraphQL: FC = memo(() => {
+  const { data, state } = useQuery("POST", async () => {
+    await graphQLClient.request(gql`
+      query {
+        viewer {
+          login
+          name
+          email
+          createdAt
+        }
+      }
+    `);
+  });
+  console.log(state);
+
   const navigate = useNavigate();
   const onClickHome = useCallback(() => {
     navigate("/");
   }, [navigate]);
+
   return (
     <>
       <p>This is QueryCheckGraphQLPage</p>
