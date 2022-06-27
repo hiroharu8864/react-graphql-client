4d7dcdfb576daf04e9541ab895b84bc114a25c68
Hiroharu Tanaka <hiroharu8864@gol.com>
2022-06-27 13:38:40 +0900

    SWR一部修正

--- src/components/Pages/FetchSWRCheck.tsx
+++ src/components/Pages/FetchSWRCheck.tsx
@@ -1,13 +1,63 @@
-import { FC, memo, useCallback } from "react";
+import { FC, memo, useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useSWR from "swr";
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+type Users = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
+
+const fetcher = async (url: string): Promise<boolean | null> => {
+  const resonse = await fetch(url);
+  return resonse.json();
+};
+
+const ResultComponent = () => {
+  const { data, error } = useSWR(
+    "https://jsonplaceholder.typicode.com/users",
+    fetcher
+  );
+
+  console.log(data);
+  console.log(error);
+
+  const [users, setUsers] = useState<Array<Users>>([]);
+
+  useEffect(() => {
+    fetcher().then((data) => {
+      setUsers(data);
+    });
+  }, []);
+
+  return (
+    <div>
+      {users.map((user) => (
+        <div key={user.id}>{user.name}</div>
+      ))}
+    </div>
+  );
+};
 
 export const FetchSWRCheck: FC = memo(() => {
-  const result = useSWR("https://jsonplaceholder.typicode.com/users", fetcher);
-  console.log(result);
-
   const navigate = useNavigate();
   const onClickHome = useCallback(() => {
     navigate("/");
@@ -15,7 +65,7 @@ export const FetchSWRCheck: FC = memo(() => {
 
   return (
     <>
-      <p>Test Page</p>
+      <ResultComponent />
       <button onClick={onClickHome}>to home</button>
     </>
   );
